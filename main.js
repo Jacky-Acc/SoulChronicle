413db4e53c5be245b55f2ebde4ba7bdad22da4db
Jacky-Acc <majielong970325@gmail.com>
2026-07-31 16:49:46 +0800

    change

--- main.js
+++ main.js
@@ -8,13 +8,6 @@ from "./src/engine/GameEngine.js";
 import LoadingScene 
 from "./src/scenes/LoadingScene.js";
 
-import GameEngine 
-from "./src/engine/GameEngine.js";
-
-
-import LoadingScene 
-from "./src/scenes/LoadingScene.js";
-
 
 
 const canvas =
